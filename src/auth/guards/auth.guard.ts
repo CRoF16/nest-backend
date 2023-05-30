341eb2dd5ead3904c3b42c11cf12b95a0c6bca27
Carlos <crf161297@gmail.com>
2023-05-30 11:53:50 -0500

    Fin de seccion 25 - nest backend

---
+++ src/auth/guards/auth.guard.ts
@@ -0,0 +1,45 @@
+import { JwtService } from '@nestjs/jwt';
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtPayload } from '../interfaces/jwt-payload';
+import { AuthService } from '../auth.service';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+
+  constructor(
+    private jwtService: JwtService,
+    private authService: AuthService,
+  ) { }
+  async canActivate(
+    context: ExecutionContext,
+  ): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+    if (!token) {
+      throw new UnauthorizedException('There is no bearer token');
+    }
+    try {
+      const payload = await this.jwtService.verifyAsync<JwtPayload>(
+        token,
+        {
+          secret: process.env.JWT_SEED
+        }
+      );
+      const user = await this.authService.findUserById(payload.id);
+      if (!user) throw new UnauthorizedException('user does not exists')
+      if (!user.isActive) throw new UnauthorizedException('user is not active')
+      request['user'] = user;
+
+    } catch (error) {
+      throw new UnauthorizedException('Invalid bearer token');
+    }
+
+
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers['authorization']?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+}
